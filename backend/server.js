dbb227fcaf44dbf7bfa7c416276f55607b9c010f
juliacsramos <julia.ramos@iclass.com.br>
2025-11-18 09:17:21 -0300

    feat: conserta commit anterior

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { USERS_LIST_BD } = require('./utils/users-list-bd');
+const { generateTokenOnLogin } = require('./utils/jwt-manager');
+const { authenticateToken } = require('./middlewares/authenticate-token');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para analisar o corpo das requisições
+app.use(bodyParser.json());
+
+// Usar o middleware cors para permitir todas as origens
+app.use(cors());
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const USER_FOUND = 
+        USERS_LIST_BD.find(user => user.username === username && user.password === password);
+
+    if(!USER_FOUND) {
+        return res.status(401).json({ message: 'Invalid credentials.' });
+    }
+
+    const userToken = generateTokenOnLogin(username);
+
+    return res.json({ token: userToken });
+});
+
+app.post('/validate-token', authenticateToken, (req, res) => {
+    res.json({ message: 'Token Válido' });
+});
+
+app.listen(PORT, () => {
+    console.log(`O Servidor está rodando no http://localhost:${PORT}`);
+});
---
+++ backend/utils/jwt-manager.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'CURSO_ANGULAR';
+
+const generateTokenOnLogin = (username) => {
+    return jwt.sign({ username }, SECRET_KEY, { expiresIn: 300 });
+};
+
+const validateToken = (token) => {
+    let TOKEN_IS_VALID = false;
+
+    try {
+        if(!token) {
+            throw new Error('Empty token.');
+        }
+
+        jwt.verify(token, SECRET_KEY);
+
+        TOKEN_IS_VALID = true;
+    } catch (error) {
+        TOKEN_IS_VALID = false;
+    }
+
+    return TOKEN_IS_VALID;
+};
+
+module.exports = { generateTokenOnLogin, validateToken };
+
